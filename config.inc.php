b0fa6875991f0eb0b9e4f10c723dde3129408819
硬硬屎 <2481729398@qq.com>
2024-11-28 19:52:38 +0800

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,30 @@
+<?php
+// site root path
+define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
+
+// plugin directory (relative path)
+define('__TYPECHO_PLUGIN_DIR__', '/usr/plugins');
+
+// theme directory (relative path)
+define('__TYPECHO_THEME_DIR__', '/usr/themes');
+
+// admin directory (relative path)
+define('__TYPECHO_ADMIN_DIR__', '/admin/');
+
+// register autoload
+require_once __TYPECHO_ROOT_DIR__ . '/var/Typecho/Common.php';
+
+// init
+\Typecho\Common::init();
+
+// config db
+$db = new \Typecho\Db('Pgsql', 'typecho_');
+$db->addServer(array (
+  'host' => 'ep-fancy-rice-a4tpo2vb.us-east-1.aws.neon.tech',
+  'port' => 5432,
+  'user' => 'neondb_owner',
+  'password' => 'jiYUFSr4ML5x',
+  'charset' => 'utf8',
+  'database' => 'neondb',
+), \Typecho\Db::READ | \Typecho\Db::WRITE);
+\Typecho\Db::set($db);
